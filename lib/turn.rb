0d3a7a8dccd21566411a58356d593a45901403e7
Aamir Kalimi <habibpramanik1998@gmail.com>
2018-11-19 15:32:45 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+#To display the board display_board method
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
